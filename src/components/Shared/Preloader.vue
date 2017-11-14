38d54e50fc6e38d051b143dc851db9f2d0668420
Andy Gillon <aawgillon@gmail.com>
2017-11-14 17:56:15 +0000

    added preloader component

---
+++ src/components/Shared/Preloader.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="preloader">
+  <h1>LOADING</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      Prop : 1
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#preloader {
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: block;
+  width: 100%;
+  height: 100%;
+  background: #ccc;
+  z-index: 1000;
+  h1 {
+    position: absolute;
+    top: 50vh;
+    width: 100%;
+  }
+}
+</style>
